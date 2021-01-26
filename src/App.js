745b6fcbb162c9f6f5228825fe05569b6c66c5ee
William Lumbantobing <william.sl.tobing@gmail.com>
2021-01-26 18:23:50 +0700

    static ui finished

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: "Mantap",
-      day: "Senen 8 Mhater",
+      text: "Memancing Ikan di Dapur",
+      day: "Senen, 8 Mharet 2000",
       reminder: true,
     },
     {
       id: 2,
-      text: "Banget",
-      day: "Jumahat 10 AgusHarimurti",
+      text: "Memakan Sate Abang Adek",
+      day: "Jumahat, 10 AgusHarimurti 2077",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Mencagkul Tobat di Kampung",
+      day: "Rebo, 13 Martumpol 1945",
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: "Mencuri Ikan tapi Bayar",
+      day: "Marminggu, 30 Februari 2020",
       reminder: false,
     },
   ]);
@@ -43,8 +56,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
