ec0e3256576a288b7c9ac211e60886c97eb2ac21
William Lumbantobing <william.sl.tobing@gmail.com>
2021-01-26 22:22:22 +0700

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
@@ -91,6 +92,7 @@ function App() {
       ) : (
         "No Tasks to Show"
       )}
+      <Footer />
     </div>
   );
 }
